e2edbe3e5a7be4d9938c6e111724fa4c8b62f585
Florian_Lehmerhofer <146080997+FlotschiL@users.noreply.github.com>
2025-09-11 12:52:22 +0200

    Monitor test1

--- Thread-Safety/Program.cs
+++ Thread-Safety/Program.cs
@@ -1,23 +1,26 @@
-﻿
-object locker = new object();  
-new Thread(new ThreadStart(() => Spieler1())).Start();
+﻿new Thread(new ThreadStart(() => Spieler1())).Start();
+new Thread(new ThreadStart(() => Spieler2())).Start();
+object ping = new object();
+object pong = new object();
 void Spieler1()
 {
     while (true)
     {
-        lock (locker)
+        Monitor.Enter(ping);
         {
             Console.WriteLine("Ping");
         }
+        Monitor.Exit(ping);
     }
 }
 void Spieler2()
 {
     while (true)
     {
-        lock (locker)
+        Monitor.Enter(ping);
         {
             Console.WriteLine("Pong");
         }
+        Monitor.Exit(ping);
     }
 }
