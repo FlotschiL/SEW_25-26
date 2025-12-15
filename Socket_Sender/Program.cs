2c389946d44579d6836eb225f2d46f0ed9e15b6e
Florian_Lehmerhofer <146080997+FlotschiL@users.noreply.github.com>
2025-12-15 13:48:37 +0100

    implemented variable messages

--- Socket_Sender/Program.cs
+++ Socket_Sender/Program.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 class TcpClientApp
 {
-    static void Main()
+    static void Main(string[] args)
     {
         string serverIp = "127.0.0.1";
         int port = 5000;
@@ -14,7 +14,7 @@ class TcpClientApp
 
         using NetworkStream stream = client.GetStream();
 
-        string message = "Hello from TCP client!";
+        string message = args[0];
         byte[] data = Encoding.UTF8.GetBytes(message);
 
         stream.Write(data, 0, data.Length);
