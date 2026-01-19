6249d9470b3afa4bc3c8b5ce0f42f9023a6f63fc
Florian_Lehmerhofer <146080997+FlotschiL@users.noreply.github.com>
2026-01-19 15:20:55 +0100

    finished the simple example

--- Sockets/Program.cs
+++ Sockets/Program.cs
@@ -5,6 +5,7 @@ class TcpServer
     public static void Main(string[] args)
     {
         //FirstSocket s = new FirstSocket();
+        //valid browser HTTP
         SimpleHttpServer server = new SimpleHttpServer();
         server.Start();
     }
